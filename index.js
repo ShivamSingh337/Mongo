9d0813a968d4daf91b8cfda67d837efa0116ca4f
ShivamSingh337 <sshivamsingh733@gmail.com>
2025-10-27 23:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+let url = "https://localhost:8080/users"
+
+// mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main()
+.then(() => {
+    console.log("connection successful")
+})
+.catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
