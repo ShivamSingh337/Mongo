3121372ef06dbbff23a088ab2a67879b553472e7
ShivamSingh337 <sshivamsingh733@gmail.com>
2025-10-29 22:05:04 +0530

    Update books.js

--- books.js
+++ books.js
@@ -13,21 +13,34 @@ const bookSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,  //this work as NOT NULL
+        maxLength: 20,
     },
     author: {
         type: String,
     },
     price: {
         type: Number,
+        min: 1,
     },
+    discount: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: String,
+        enum: ["fiction", "non-fiction"],
+    },
+     genre: [String]
 });
 
 const Book = mongoose.model("Book", bookSchema);
 
 let book1 = new Book({
     title: "Matematics",
-    author: "RD Sharma",
-    price: 200,
+    // author: "RD Sharma",
+    price: 500,
+    category: "fiction",
+    genre: ["comic", "superheroes", "fiction"],
 }); 
 book1.save().then(res => {
     console.log(res);
